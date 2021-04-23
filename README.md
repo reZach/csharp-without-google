9b263b60b74a4cdfcaaf8104133a298b85ddd11f
reZach <reZach@users.noreply.github.com>
2021-04-22 23:50:41 -0500

    first challenge

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # csharp-without-google
-Can you finish?
+Can you finish? Are you able to write C# _without_ Google? Let's test your skills and help you become a better software developer in the process!
+
+
+## Challenges
+1. Write a command line application that accepts a user-inputted first and last name and returns the value in Capital Case.
---
+++ challenges/1/ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Name, please: ");
+
+            string name = Console.ReadLine();
+            name = name.ToLower();
+
+            int current = 0;
+            char[] characters = name.ToCharArray();
+            for (int i = 0; i < characters.Length; i++)
+            {
+                // Convert to uppercase for first character,
+                // and any letter following a space
+                current = (int)name[i];
+                if (i == 0 || ((int)name[i - 1] == 32 && current >= 97 && current <= 122))
+                {
+                    characters[i] = (char)((int)characters[i] - 32);
+                }
+            }
+
+            Console.WriteLine($"Thank you {new string(characters)}!");
+        }
+    }
+}
