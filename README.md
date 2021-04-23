b95f079f30db2a22e585f69640324987d73772dc
reZach <reZach@users.noreply.github.com>
2021-04-23 00:14:30 -0500

    partial

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ Can you finish? Are you able to write C# _without_ Google? Let's test your skill
 ## Challenges
 1. Write a command line application that accepts a user-inputted first and last name and returns the value in Capital Case.
 
-2. Create an extension method for a string that reverses the string order.
+2. Write a command line application that takes a string variable, reverses the characters in the string with an extension method, and prints the result.
+
+3. Write a command line application that contains a list of `Person`. `Person` must have at least 3 properties that a person may have (ie. age, full name, etc.). The list of `Person` should be written to a .json file.
---
+++ challenges/3/ConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>
+            {
+                new Person
+                {
+                    FullName = "Jonny Hopkins",
+                    Age = 33,
+                    Gender = Gender.Male
+                },
+                new Person
+                {
+                    FullName = "Ashley Ricker",
+                    Age = 51,
+                    Gender = Gender.Female
+                }
+            };
+
+            Json
+
+            File.WriteAllBytes
+        }
+    }
+
+    public class Person
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
